3380db3beb23f3b56bde8650fa1ac0eb198c0ff1
Brian <brian.lagaude@gmail.com>
2020-03-25 16:20:31 +0100

    essai 3: 2e atelier ajouté

--- wilders.js
+++ wilders.js
@@ -1,5 +1,19 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+    response.writeHead(200);
+    response.end('Hello World');
+});
+
+server.listen(3000);
+console.log('Adresse du serveur: http://localhost:3000');
+
 const chalk = require('chalk');
 
 console.log(chalk.blue('Lucas')+ ' et ' + chalk.red('Martin') + ' et ' + chalk.yellow('Angélique') + ' et ' + chalk.green('Stéphaine'));
+
+
+
+
 
 
